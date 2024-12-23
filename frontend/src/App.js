c012041be04a0f6285887f257d75fbdad36afddc
Paul-Steve-Mithun <paulstevemithun@gmail.com>
2024-12-23 19:21:00 +0530

    titletag/icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,3 @@
-// import React, { useState } from "react";
-// import FileUploader from "./components/FileUploader";
-// import Dashboard from "./components/Dashboard"; // Import the Dashboard
-// import ReportDownloader from "./components/ReportDownloader";
-// import "./styles.css";
-
-// function App() {
-//   const [analysisResults, setAnalysisResults] = useState([]);
-
-//   return (
-//     <div className="app">
-//       <h1>Sentiment Analysis Portal</h1>
-//       <FileUploader setAnalysisResults={setAnalysisResults} />
-//       {analysisResults.length > 0 && (
-//         <>
-//           <Dashboard results={analysisResults} /> {/* Use Dashboard here */}
-//           <ReportDownloader results={analysisResults} />
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useState, useRef } from "react";
 import FileUploader from "./components/FileUploader";
 import SentimentChart from "./components/SentimentChart";
